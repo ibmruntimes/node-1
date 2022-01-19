baec1feb958e25cd4697b0916d976234ab3963de
ALEXY <alex.yung@ibm.com>
2022-01-19 02:58:38 -0500

    tools: use /bin/sh and add sdwrap

--- tools/zos/modifysidedeck.sh
+++ tools/zos/modifysidedeck.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 if [ "$#" -ne 3 ] || ! [ -f "$1" ]; then
   echo ===========================
@@ -12,19 +12,22 @@ originalsidedeck=$1
 outputsidedeck=$2
 newdllname=$3
 
+SCRIPT_DIR=$(dirname $0)
 ID=`date +%C%y%m%d_%H%M%S`
-TMP="/tmp/sidedeck-$(basename $0).$ID.tmp"
-TMP2="/tmp/sidedeck-$(basename $0).$ID.tmp.2"
+TMP="/tmp/sidedeck-$(basename "$0").$ID.tmp"
+TMP2="/tmp/sidedeck-$(basename "$0").$ID.tmp.2"
 
 # Remove on exit/interrupt
-trap "/bin/rm -rf $TMP $TMP2 && exit" EXIT INT TERM QUIT HUP
+trap '/bin/rm -rf "$TMP" "$TMP2" && exit' EXIT INT TERM QUIT HUP
 
 set -x
-dd conv=unblock cbs=80 if=$originalsidedeck of=$TMP
-chtag -tc 1047 $TMP
-cat $TMP | sdwrap -u | sed -e "s/\(^ IMPORT \(DATA\|CODE\)64,\)'[^']*'/\1'$newdllname'/g"  | sdwrap -w > $TMP2
+dd conv=unblock cbs=80 if="$originalsidedeck" of="$TMP"
+chtag -tc 1047 "$TMP"
+${SCRIPT_DIR}/sdwrap.py -u -i "$TMP" -o "$TMP2"
+chtag -tc 819 "$TMP2"
+sed -e "s/\(^ IMPORT \(DATA\|CODE\)64,\)'[^']*'/\1'$newdllname'/g" "$TMP2" > "$TMP"
+${SCRIPT_DIR}/sdwrap.py -i "$TMP" -o "$TMP2"
 
 # Reformat sidedeck to be USS compatible
-iconv -f ISO8859-1 -t IBM-1047 $TMP2 > $TMP
-mv $TMP $TMP2
-dd conv=block cbs=80 if=${TMP2} of=$outputsidedeck
+iconv -f ISO8859-1 -t IBM-1047 "$TMP2" > "$TMP"
+dd conv=block cbs=80 if="$TMP" of="$outputsidedeck"
